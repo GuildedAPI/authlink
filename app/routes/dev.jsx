5b1e3a7bbaa982e0f04d6798b111b2c2334da914
shay <bakkcat@gmail.com>
2022-05-18 20:13:17 +0200

    Remove unused menu button

--- app/routes/dev.jsx
+++ app/routes/dev.jsx
@@ -12,16 +12,6 @@ export default function Developers() {
             <div className='w-full mb-4 sm:w-32 sm:mb-0 sm:mr-4 bg-guilded-slate p-2 rounded h-full' id='sidebar'>
                 <div className='flex text-guilded-subtitle text-xs'>
                     <h3 className='font-bold'>Menu</h3>
-                    {/*<button
-                        className='ml-auto'
-                        onClick={() => {
-                            const sidebar = document.getElementById('sidebar')
-                            const open = !sidebar.classList.contains('w-0')
-                            open ? sidebar.classList.add('w-0') : sidebar.classList.remove('w-0')
-                        }}
-                    >
-                        <i className='ci-circle_chevron_left'/>
-                    </button>*/}
                 </div>
                 <ul className='text-guilded-link text-sm'>
                     <li><Link to='/dev/apps'>Your Apps</Link></li>
